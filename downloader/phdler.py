6a5c3f7e706572f30ecf459de59069638e583edf
pishinbfi3 <farhamgamer88@gmail.com>
2026-04-23 00:54:53 +0330

    Update phdler.py

--- downloader/phdler.py
+++ downloader/phdler.py
@@ -1,55 +1,19 @@
-#!/usr/bin/env python
-from functions import *
+import sys
+from functions import process_download
 
 def main():
+    if len(sys.argv) < 2:
+        print("Usage: phdler.py <url>")
+        sys.exit(1)
 
-    check_for_database()
+    url = sys.argv[1]
+    dest = "downloads"
 
-    if len(sys.argv) > 1:
+    parts = process_download(url, dest)
 
-        if sys.argv[1] == "start":
-            dl_start()
-            
-        elif sys.argv[1] == "custom":
-            if len(sys.argv) > 2:
-                custom_dl(sys.argv[2])
-            else:
-                how_to_use("Missing item")
+    print("DONE PARTS:")
+    for p in parts:
+        print(p)
 
-        elif sys.argv[1] == "add":
-            if len(sys.argv) > 2:
-                add_check(sys.argv[2])
-            else:
-                how_to_use("Missing item")
-
-        elif sys.argv[1] == "delete":
-            if len(sys.argv) > 2:
-                type_check(sys.argv[2])
-                list_items(sys.argv[2])
-                u_input = input("Please enter the ID to delete (or c to cancel): ")
-                if u_input == "c":
-                    print("Operation canceled.")
-                else:
-                    delete_item(u_input)
-            else:
-                how_to_use("Missing item")
-
-        elif sys.argv[1] == "list":
-            if len(sys.argv) > 2:
-                type_check(sys.argv[2])
-                list_items(sys.argv[2])
-            else:
-                how_to_use("Missing item")
-
-        elif sys.argv[1] == "help":
-            help_command()
-
-        else:
-            how_to_use("Command not found!")
-
-    else:
-        how_to_use("Missing command.")
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
